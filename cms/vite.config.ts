104894302ec4bb153dc8827fe90cbe36cc2f111f
deepakkainskep <deepak.gupta@kainskep.com>
2026-08-19 13:34:19 +0530

    fix strict port issue in cms

--- cms/vite.config.ts
+++ cms/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     allowedHosts: true,
-    strictPort: true,
+    strictPort: false,
   },
 });
